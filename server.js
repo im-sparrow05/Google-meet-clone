af09e34951208b281627cddfbb3ad55d891fabb0
Rahul <rahulkr763386@gmail.com>
2023-07-09 00:18:47 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,177 @@
+const express = require("express");
+const path = require("path");
+var app = express();
+var server = app.listen(3000, function () {
+  console.log("Listening on port 3000");
+});
+const fs = require("fs");
+const fileUpload = require("express-fileupload");
+const io = require("socket.io")(server, {
+  allowEIO3: true, // false by default
+});
+app.use(express.static(path.join(__dirname, "")));
+var userConnections = [];
+io.on("connection", (socket) => {
+  console.log("socket id is ", socket.id);
+  socket.on("userconnect", (data) => {
+    console.log("userconnent", data.displayName, data.meetingid);
+    var other_users = userConnections.filter(
+      (p) => p.meeting_id == data.meetingid
+    );
+    userConnections.push({
+      connectionId: socket.id,
+      user_id: data.displayName,
+      meeting_id: data.meetingid,
+    });
+    var userCount = userConnections.length;
+    console.log(userCount);
+    other_users.forEach((v) => {
+      socket.to(v.connectionId).emit("inform_others_about_me", {
+        other_user_id: data.displayName,
+        connId: socket.id,
+        userNumber: userCount,
+      });
+    });
+    socket.emit("inform_me_about_other_user", other_users);
+  });
+  socket.on("SDPProcess", (data) => {
+    socket.to(data.to_connid).emit("SDPProcess", {
+      message: data.message,
+      from_connid: socket.id,
+    });
+  });
+  socket.on("sendMessage", (msg) => {
+    console.log(msg);
+    var mUser = userConnections.find((p) => p.connectionId == socket.id);
+    if (mUser) {
+      var meetingid = mUser.meeting_id;
+      var from = mUser.user_id;
+      var list = userConnections.filter((p) => p.meeting_id == meetingid);
+      list.forEach((v) => {
+        socket.to(v.connectionId).emit("showChatMessage", {
+          from: from,
+          message: msg,
+        });
+      });
+    }
+  });
+  socket.on("fileTransferToOther", (msg) => {
+    console.log(msg);
+    var mUser = userConnections.find((p) => p.connectionId == socket.id);
+    if (mUser) {
+      var meetingid = mUser.meeting_id;
+      var from = mUser.user_id;
+      var list = userConnections.filter((p) => p.meeting_id == meetingid);
+      list.forEach((v) => {
+        socket.to(v.connectionId).emit("showFileMessage", {
+          username: msg.username,
+          meetingid: msg.meetingid,
+          filePath: msg.filePath,
+          fileName: msg.fileName,
+        });
+      });
+    }
+  });
+
+  socket.on("disconnect", function () {
+    console.log("Disconnected");
+    var disUser = userConnections.find((p) => p.connectionId == socket.id);
+    if (disUser) {
+      var meetingid = disUser.meeting_id;
+      userConnections = userConnections.filter(
+        (p) => p.connectionId != socket.id
+      );
+      var list = userConnections.filter((p) => p.meeting_id == meetingid);
+      list.forEach((v) => {
+        var userNumberAfUserLeave = userConnections.length;
+        socket.to(v.connectionId).emit("inform_other_about_disconnected_user", {
+          connId: socket.id,
+          uNumber: userNumberAfUserLeave,
+        });
+      });
+    }
+  });
+
+  // <!-- .....................HandRaise .................-->
+  socket.on("sendHandRaise", function (data) {
+    var senderID = userConnections.find((p) => p.connectionId == socket.id);
+    console.log("senderID :", senderID.meeting_id);
+    if (senderID.meeting_id) {
+      var meetingid = senderID.meeting_id;
+      // userConnections = userConnections.filter(
+      //   (p) => p.connectionId != socket.id
+      // );
+      var list = userConnections.filter((p) => p.meeting_id == meetingid);
+      list.forEach((v) => {
+        var userNumberAfUserLeave = userConnections.length;
+        socket.to(v.connectionId).emit("HandRaise_info_for_others", {
+          connId: socket.id,
+          handRaise: data,
+        });
+      });
+    }
+  });
+  // <!-- .....................HandRaise .................-->
+});
+
+app.use(fileUpload());
+app.post("/attachimg", function (req, res) {
+  var data = req.body;
+  var imageFile = req.files.zipfile;
+  console.log(imageFile);
+  var dir = "public/attachment/" + data.meeting_id + "/";
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+
+  imageFile.mv(
+    "public/attachment/" + data.meeting_id + "/" + imageFile.name,
+    function (error) {
+      if (error) {
+        console.log("couldn't upload the image file , error: ", error);
+      } else {
+        console.log("Image file successfully uploaded");
+      }
+    }
+  );
+});
+
+const WebSocket = require("ws");
+
+// Keep track of connected clients
+const clients = new Set();
+
+// Handle WebSocket connection event
+server.on("connection", function connection(ws) {
+  clients.add(ws);
+
+  // Handle WebSocket message event
+  ws.on("message", function incoming(message) {
+    // Broadcast the new member join event to all connected clients
+    clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify({ type: "newMemberJoined" }));
+      }
+    });
+  });
+
+  // Handle WebSocket connection close event
+  ws.on("close", function close() {
+    clients.delete(ws);
+  });
+});
+
+
+
+// Event listener for new WebSocket connections
+server.on('connection', function (ws) {
+  // Event listener for receiving drawing data from a client
+  server.on('message', function (message) {
+    // Broadcast the drawing data to all connected clients (except the sender)
+    wss.clients.forEach(function (client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
